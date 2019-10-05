2bb586c3f4d2b1236044fb63480cdeee77a776b0
Kosyrev Serge <serge.kosyrev@iohk.io>
2019-10-05 03:43:31 +0300

    Supply the testcase branch generator.

---
+++ tests/mk.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+set -eu
+
+NO_GENERATE=
+NO_PRECLEAN=
+NO_POSTCLEAN=
+SAME_BRANCH=
+ID=
+while true; do case "$1" in
+                       --no-generate ) NO_GENERATE=1; shift;;
+                       --no-preclean ) NO_PRECLEAN=1; shift;;
+                       --no-postclean ) NO_POSTCLEAN=1; shift;;
+                       --same-branch ) SAME_BRANCH=1; shift;;
+                       --id ) ID="$2"; shift 2;;
+                       -v | --verbose ) set -x; shift;;
+                       * ) break; esac; done
+
+prv=$(mktemp)
+pub=${prv}.pub
+tmp=$(mktemp)
+atexit() {
+        rm -f ${prv} ${pub} ${tmp}
+}
+trap atexit EXIT
+
+if test -z "${NO_PRECLEAN}"
+then git checkout "master"
+     git reset --hard
+     git clean -f; fi
+
+if test -z "${ID}"
+then jcli key generate    --type ed25519 ${prv}
+     jcli key to-public          --input ${prv} ${pub}; fi
+
+id=${ID:-$(cut -d_ -f2 ${pub} | head -n1)}
+
+JSONtransform="$1"
+SHtransform="$2"
+branch="$3"
+message="${4:-NUON:  new}"
+ticker=${5:-NUON}
+
+if test -z "${NO_GENERATE}"
+then registry prepare-submission \
+       --public-key-file ${pub} \
+       --ticker ${ticker} \
+       --pool-web https://12345 &&
+       if test ! -f ${id}.json
+       then echo "ERROR: ${id}.json wasn't created!" >&2; exit 1; fi; fi
+
+if test -n "$JSONtransform"
+then jq  --indent 4 --join-output \
+        "$JSONtransform" > ${tmp} < ${id}.json &&
+     mv -f                 ${tmp}   ${id}.json; fi
+
+if test -z "${NO_GENERATE}"
+then echo 1                       > ${id}.sig; fi
+
+if test -n "$SHtransform"
+then eval ${SHtransform}; fi
+
+if test -n "${branch}" -a -n "${message}" || test -n "${SAME_BRANCH}"
+then if test -z "${SAME_BRANCH}"
+     then git checkout -b "${branch}"; fi
+
+     git add --all
+     git commit -m "${message}"
+     
+     if test -z "${NO_POSTCLEAN}"
+     then git checkout master; fi; fi
---
+++ tests/mkall.sh
@@ -0,0 +1,123 @@
+#!/bin/sh
+
+set -e
+
+git clean -f
+git checkout master
+git branch | grep -v master | sed 's/*//g' | xargs git branch -D || true
+def="$(dirname $0)/mk.sh"
+inc='i=$((i+1)); total=$((total+1))'
+total=0
+
+###
+### Valid cases
+###
+$def  '' '' \
+"00-new"
+
+f=$(ls *.json | head -n1 | xargs echo)
+id=$(jq .id <$f | xargs echo | cut -d_ -f2)
+echo f=${f} id=${id}
+$def --no-generate --id "${id}" '.ticker="OTHR"' 'echo 2 > ${id}.sig' \
+"01-update"       "OTHR:  update"
+
+$def  '.pledge_address="ed25519_pk1uadm2958qvrta7e0j275ftrxtth5p9txaqq3gg9z5fxg0zxq0d3sa9yyys"' '' \
+"02-pledging"
+
+###
+### Invalid cases
+###
+now=$i
+start=9 # That's right, 9, not 10.
+skipped=$((start-now))
+i=${start}
+
+###
+### change structure
+###
+f=$(ls *.json | head -n1 | xargs echo)
+id=$(jq .id <$f | xargs echo | cut -d_ -f2)
+echo f=${f} id=${id}
+eval $inc; $def --no-generate --id "${id}" '' "ls; mv ${id}"'.{json,sig}' \
+"$i-delete-and-modify" "$i: Delete! And! Modify!"
+
+eval $inc; $def --no-generate '' 'rm *.json' \
+"$i-drop-files"
+
+eval $inc; $def  '' '' \
+"$i-commit-message" "Sandals of God"
+
+set -x
+eval $inc; $def --no-postclean  '' '' \
+"$i-history-too-long" # Generate first commit
+$def --no-preclean --same-branch '' '' \
+"" "SECD:  new" "SECD"
+
+###
+### file hierarchy
+###
+eval $inc; $def --no-generate '' 'touch ../{somewhere,something}' \
+"$i-not-in-registry"
+
+eval $inc; $def --no-generate  '' 'mkdir somewhere; touch somewhere/{else,and.again}' \
+"$i-not-in-registry-either"
+
+eval $inc; $def  '' 'cp ${id}.json ${id}1.json' \
+"$i-too-many-files"
+
+###
+### file names
+###
+eval $inc; $def  '' 'mv ${id}.json terrible.json' \
+"$i-file-id"
+
+###
+### file content
+###
+eval $inc; $def  'empty' '' \
+"$i-empty-json"
+
+eval $inc; $def  '.unexpected="really!"' '' \
+"$i-unexpected-field"
+
+eval $inc; $def  'del(.homepage)' '' \
+"$i-too-few-fields"
+
+###
+### file content: Id
+###
+eval $inc; $def  'del(.id)' '' \
+"$i-missing-mandatory"
+
+eval $inc; $def  '.id=3.141592653589793' '' \
+"$i-mandatory-field-type"
+
+eval $inc; $def  '.id="ed25519_nsacrypto"' '' \
+"$i-internal-id-length"
+
+eval $inc; $def  '.id="nsacrypto_pk1dqaghc0luz0m9mr807hyaeprw49rx8ykvpcw6vvldckznl0q0y4qr280s2"' '' \
+"$i-internal-id-format"
+
+eval $inc; $def  '.id="ed25519_pk1dqaghc0luz0m9mr807hyaeprw49rx8ykvpcw6vvldckznl0q0y4qr280s2"' '' \
+"$i-id-entry-mismatch"
+
+###
+### file content: types
+###
+eval $inc; $def  '.pledge_address=2.7182818284590' '' \
+"$i-pledge-field-type"
+
+eval $inc; $def  '.ticker="ZALMAY"' '' \
+"$i-ticker-too-long"
+
+eval $inc; $def  '.ticker="NO"' '' \
+"$i-ticker-too-short"
+
+eval $inc; $def  '.ticker="FST"' '' \
+"$i-duplicate-ticker"
+
+cat <<EOF
+
+Generated:  $total total branches
+
+EOF
